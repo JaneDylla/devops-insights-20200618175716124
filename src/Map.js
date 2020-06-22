3f0cefa69a67b4a5e2ba83ec3b17f51e83dfcd89
Jane Dylla <dylla027@d.umn.edu>
2020-06-22 22:40:15 +0000

    V2: build failed. Added code to try and fix

--- src/Map.js
+++ src/Map.js
@@ -5,17 +5,10 @@ import AppHeader from './components/AppHeader';
 import MapContainer from './containers/MapContainer';
 import './Map.css';
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -40.901, lng: 174.886 },
-    zoom: 7
-  });
-}
 
 // Create the script tag, set the appropriate attributes
 var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAbDo7LW63FwTAjdR3Afq0sd44whQcYW_4&callback=initMap';
 script.defer = true;
 script.async = true;
 
@@ -26,5 +19,13 @@ window.initMap = function() {
 
 // Append the 'script' element to 'head'
 document.head.appendChild(script);
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -40.901, lng: 174.886 },
+    zoom: 7
+  });
+}
 
 export default initMap;
