99624b95921ed5aff7fb0930ba403a136d1f305a
Jane Dylla <dylla027@d.umn.edu>
2020-06-22 22:32:12 +0000

    V2: Another attempt to get the map to show up

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+*/
 
-/*
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Map.css';
+import './index.css';
 import initMap from './Map';
 
 ReactDOM.render(<initMap />, document.getElementById('root'));
-*/
--- src/mapindex.js
+++ src/mapindex.js
@@ -1,6 +1,9 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Map.css';
 import initMap from './Map';
 
 ReactDOM.render(<initMap />, document.getElementById('root'));
+*/
+//may be obsolete
